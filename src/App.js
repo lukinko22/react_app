9b115ec85f7b3516ec8b01094118e88af0c20ef2
Lukas <lukinko22@yahoo.co.uk>
2023-03-08 20:25:30 +0000

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Charles is happy
         </a>
       </header>
     </div>
